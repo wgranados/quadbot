bd4b40dec1e71e7ffec31012e083bf6ce48f176b
William Granados <william.granadosmatamoros@mail.utoronto.ca>
2020-10-31 21:49:01 -0400

    slight update for integration

--- clients/discord/handler.py
+++ clients/discord/handler.py
@@ -1,10 +1,14 @@
 import discord
 import asyncio
 
+from plugins.invoker import Invoker
+from utils.details import config
+
+
 class MyClient(discord.Client):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
+        self.config = kwargs['config']
         # create the background task and run it in the background
         self.bg_task = self.loop.create_task(self.my_background_task())
 
@@ -13,6 +17,17 @@ class MyClient(discord.Client):
         print(self.user.name)
         print(self.user.id)
         print('------')
+        for guild in self.guilds:
+            print(guild.name)
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return 
+        if str(message.channel) in self.config['channels']:
+            if message.content.startswith(self.config['command_prefix']):
+                reply = await invoker.invoke_command(message)
+                await message.channel.send(reply)
+                # print(reply)
 
     async def my_background_task(self):
         await self.wait_until_ready()
@@ -20,7 +35,8 @@ class MyClient(discord.Client):
         channel = self.get_channel(137356419333750784) # channel ID goes here
         while not self.is_closed():
             counter += 1
-            await channel.send(counter)
             await asyncio.sleep(60) # task runs every 60 seconds
 
-dsclient = MyClient() 
+invoker = Invoker()
+dsclient = MyClient(config=config['discord']) 
+
